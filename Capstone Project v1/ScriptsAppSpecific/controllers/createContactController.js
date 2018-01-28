faa2708a51d4b4ad1a48ffea007e906d77c38502
mwillia95 <mwilliams10@augusta.edu>
2018-01-27 23:51:19 -0500

    added form validation to createContact form

--- Capstone Project v1/ScriptsAppSpecific/controllers/createContactController.js
+++ Capstone Project v1/ScriptsAppSpecific/controllers/createContactController.js
@@ -10,10 +10,38 @@
     };
 
     stateList();
- 
+    var phoneValidation = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/
+
     self.submit = function (valid) {
         if (!valid)
             return;
+
+        var type = self.contact.serviceType;
+        if (!type)
+        {
+            console.log({ Service: type });
+            return;
+        }
+        if (type == "email" || type == "both")
+        {
+            var email = self.contact.email;
+            if (!email || !email.trim())
+            {
+                console.log(self.contact.email);
+                return;
+            }
+        }
+        if (type == "phone" || type == "both")
+        {
+            if (!phoneValidation.text(self.contact.phone)) {
+                console.log(self.contact.phone);
+                return;
+            }
+        }
+        if (!self.contact.state)
+        {
+            console.log({ NoStateSelected });
+        }
         var contact =
             {
                 FirstName: self.contact.firstName,
