9dfdcc8e3470fea1ff6c6b348f29da71da41006e
Adam Perry <adam.perry1210@gmail.com>
2018-02-10 23:24:38 -0500

    added a shitty theme....still messing with it

--- Capstone Project v1/ScriptsAppSpecific/controllers/homeController.js
+++ Capstone Project v1/ScriptsAppSpecific/controllers/homeController.js
@@ -98,7 +98,7 @@
     function displaySearchResults(map, searchBar) {      
         var place = searchBar.getPlace();
         
-        if (place.length == 0) {
+        if (place.length === 0) {
             return;
         }
 
@@ -231,7 +231,87 @@
               elementType: 'labels.text.stroke',
               stylers: [{ color: '#17263c' }]
           }
-        ],  
+        ],
+        candy: [
+         { elementType: 'geometry', stylers: [{ color: '#ade88b' }] },  //soft green
+         { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+         { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+         {
+             featureType: 'administrative.locality',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#d59563' }]
+         },
+         {
+             featureType: 'poi',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#d59563' }]
+         },
+         {
+             featureType: 'poi.park',
+             elementType: 'geometry',
+             stylers: [{ color: '#263c3f' }]
+         },
+         {
+             featureType: 'poi.park',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#6b9a76' }]
+         },
+         {
+             featureType: 'road',
+             elementType: 'geometry',
+             stylers: [{ color: '#9a2daf' }]   //purple
+         },
+         {
+             featureType: 'road',
+             elementType: 'geometry.stroke',
+             stylers: [{ color: '#f2aeb4' }]  //pink
+         },
+         {
+             featureType: 'road',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#9ca5b3' }]
+         },
+         {
+             featureType: 'road.highway',
+             elementType: 'geometry',
+             stylers: [{ color: '#ea3545' }]  //salmon
+         },
+         {
+             featureType: 'road.highway',
+             elementType: 'geometry.stroke',
+             stylers: [{ color: '#6dcde8' }]   //baby blue
+         },
+         {
+             featureType: 'road.highway',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#f3d19c' }]
+         },
+         {
+             featureType: 'transit',
+             elementType: 'geometry',
+             stylers: [{ color: '#2f3948' }]
+         },
+         {
+             featureType: 'transit.station',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#d59563' }]
+         },
+         {
+             featureType: 'water',
+             elementType: 'geometry',
+             stylers: [{ color: '#efac40' }]  //soft yellow
+         },
+         {
+             featureType: 'water',
+             elementType: 'labels.text.fill',
+             stylers: [{ color: '#515c6d' }]
+         },
+         {
+             featureType: 'water',
+             elementType: 'labels.text.stroke',
+             stylers: [{ color: '#17263c' }]
+         }
+        ],
     };
 
     initMap();
